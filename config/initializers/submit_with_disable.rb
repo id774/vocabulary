f4ceabb88d250ea4dadf5fedaf42afc108b27cdf
id774 <idnanashi@gmail.com>
2013-09-24 23:08:28 +0900

    6 Coloring, minor fixes

--- config/initializers/submit_with_disable.rb
+++ config/initializers/submit_with_disable.rb
@@ -3,7 +3,7 @@ module ActionView
     module FormTagHelper
       alias_method :original_submit_tag, :submit_tag
       def submit_tag(value=nil, options={})
-        options[:data] = { disable_with: 'Sending...' } unless options[:data]
+        options[:data] = { disable_with: '送信中...' } unless options[:data]
         original_submit_tag(value, options)
       end
     end
